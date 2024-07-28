95175d3e3d31390202d55d0839cfd786eead49d5
Harsh Gupta <hkgupta1601@gmail.com>
2024-07-28 13:04:39 +0530

    Added Hover Effect

---
+++ lib/config/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+var bgColor = const Color(0xFF191A22);
+var containerColor = const Color(0xFF22232E);
+var fontColor = const Color(0xFFFFFFFF);
+var labelColor = const Color(0xFF767676);
+var primaryColor = const Color(0xFF00DA64);
